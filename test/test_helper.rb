cbde5a10ec74c4cf457f64968f6919932e803d95
Renaud Kern <renaud.kern@gmail.com>
2008-11-26 15:59:09 +0100

    adapte require db file for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,18 +4,18 @@ require 'active_record'
 require 'active_support/test_case'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/tramp_models')
 require File.expand_path(File.dirname(__FILE__) + '/tramp_test_helper')   #setup test
-require File.expand_path(File.dirname(__FILE__) + '/../db/create_tramp') 
+require File.expand_path(File.dirname(__FILE__) + '/../generators/tramp/templates/create_tramp') 
            
 ActiveRecord::Base.establish_connection(:adapter=>'sqlite3', :dbfile=>':memory:')
 ActiveRecord::Migration.verbose = false
-CreateTramp.migrate(:up)
+TrampMigration.migrate(:up)
 ActiveRecord::Migration.verbose = true
 
 class ActiveSupport::TestCase
   
   def setup
     ActiveRecord::Migration.verbose = false
-    CreateTramp.migrate(:up)
+    TrampMigration.migrate(:up)
     ActiveRecord::Migration.verbose = true
   end
 
