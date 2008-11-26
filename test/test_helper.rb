5d4dec7c8f31f78d895fcd9496b5514b78defcb4
Renaud Kern <renaud.kern@gmail.com>
2008-11-26 18:25:31 +0100

    add some comments in test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,9 @@ require 'test/unit'
 require 'rubygems'
 require 'active_record'
 require 'active_support/test_case'
-require File.expand_path(File.dirname(__FILE__) + '/../lib/tramp_models')
-require File.expand_path(File.dirname(__FILE__) + '/tramp_test_helper')   #setup test
-require File.expand_path(File.dirname(__FILE__) + '/../generators/tramp/templates/create_tramp') 
+require File.expand_path(File.dirname(__FILE__) + '/../lib/tramp_models')   #the models
+require File.expand_path(File.dirname(__FILE__) + '/tramp_test_helper')     #setup fixtures 
+require File.expand_path(File.dirname(__FILE__) + '/../generators/tramp/templates/create_tramp') #database
            
 ActiveRecord::Base.establish_connection(:adapter=>'sqlite3', :dbfile=>':memory:')
 ActiveRecord::Migration.verbose = false
