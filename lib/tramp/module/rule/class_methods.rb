d45e4542a867d2f41eb9f1780678ebdc6704f7a7
Renaud Kern <renaud.kern@gmail.com>
2008-12-08 11:51:13 +0100

    try add module folder

---
+++ lib/tramp/module/rule/class_methods.rb
@@ -0,0 +1,36 @@
+module Tramp
+  module Rule
+
+    module ClassMethods
+      
+      def self.included(base)
+        base.send :init_association
+      end
+    
+      def klass
+        class<<self;self;end
+      end
+      
+      def init_association
+        has_many :service_agreements, :class_name => 'Tramp::ServiceAgreement'
+      end
+
+      def parameter(name, calcul)
+          class_eval "def #{name}; #{calcul}; end\n" 
+      end
+
+      def movement(lines=[],&block)
+        container = Tramp::RuleContainer.new
+        yield container
+        define_method('entries') do
+          container.entries
+        end
+        define_method('collections') do
+          container.collections
+        end
+      end
+    
+    end
+  
+  end
+end
