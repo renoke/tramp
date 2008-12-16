27a698025b658520ae60bde46b86eeb122ac85e1
Renaud Kern <renaud.kern@gmail.com>
2008-12-16 14:39:28 +0100

    remove unecessary models

--- lib/tramp/models/event_type.rb
+++
@@ -1,8 +0,0 @@
-module Tramp
-  class EventType < ActiveRecord::Base
-  
-    has_many :events, :class_name => 'Tramp::Model::Event'
-    has_many :service_agreements, :class_name => 'Tramp::ServiceAgreement'
-  
-  end
-end
--- lib/tramp/models/party.rb
+++
@@ -1,6 +0,0 @@
-module Tramp
-  class Party < ActiveRecord::Base
-    has_and_belongs_to_many :service_agreements, :class_name=>"Tramp::ServiceAgreement" 
-    has_many :events, :class_name=>"Tramp::Model::Event"
-  end
-end
