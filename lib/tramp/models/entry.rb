7ed142821eeb59384dd92b5f9ba7ef0618c9e208
Renaud Kern <renaud.kern@gmail.com>
2008-12-18 11:46:31 +0100

    move money agregation to module

--- lib/tramp/models/entry.rb
+++ lib/tramp/models/entry.rb
@@ -5,23 +5,7 @@ module Tramp
       include Tramp::Entry::InstanceMethods
       extend  Tramp::Entry::ClassMethods
       
-      composed_of :debit, 
-                  :class_name => "Money", 
-                  :mapping => [ %w(debit amount), %w(currency currency) ],
-                  :converter => :convert,
-                  :constructor => :construct
-                  
-      composed_of :credit, 
-                  :class_name => "Money", 
-                  :mapping => [ %w(credit amount), %w(currency currency) ],
-                  :converter => :convert,
-                  :constructor => :construct
-                  
-      composed_of :amount, 
-                  :class_name => "Money", 
-                  :mapping => [ %w(amount amount), %w(currency currency) ],
-                  :converter => :convert,
-                  :constructor => :construct
+
     end
   end
 end
--- lib/tramp/module/entry/class_methods.rb
+++ lib/tramp/module/entry/class_methods.rb
@@ -8,6 +8,24 @@ module Tramp
       
       def init_class_methods
         belongs_to :movement, :class_name=>'Tramp::Model::Movement'
+        
+        composed_of :debit, 
+                    :class_name => "Money", 
+                    :mapping => [ %w(debit amount), %w(currency currency) ],
+                    :converter => :convert,
+                    :constructor => :construct
+
+        composed_of :credit, 
+                    :class_name => "Money", 
+                    :mapping => [ %w(credit amount), %w(currency currency) ],
+                    :converter => :convert,
+                    :constructor => :construct
+
+        composed_of :amount, 
+                    :class_name => "Money", 
+                    :mapping => [ %w(amount amount), %w(currency currency) ],
+                    :converter => :convert,
+                    :constructor => :construct
       end
       
     end
